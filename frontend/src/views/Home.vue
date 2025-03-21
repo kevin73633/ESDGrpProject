16856f9bd7ee5530ae9ed1ebcdea04db5e8541fc
kevin73633 <kevindeepak@hotmail.com>
2025-03-21 17:57:58 +0800

    Update Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -399,7 +399,7 @@ export default {
     }
   },
   mounted() {
-    axios.get('/user')
+    axios.get("http://127.0.0.1:5001/user")
     .then((response) => {
       console.log(response);
     }, (error) => {
