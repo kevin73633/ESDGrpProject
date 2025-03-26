3fccc6a9b8545ae63244612d5c63200b9aab68c6
kevin73633 <kevindeepak@hotmail.com>
2025-03-26 21:55:03 +0800

    fixed gitignore

--- chatgpt.py
+++ chatgpt.py
@@ -22,7 +22,7 @@ CORS(app)
 
 # Connect to the Project database
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-     environ.get("dbURL") or "mysql+mysqlconnector://root:root@localhost:3306/Project"
+     environ.get("dbURL") or "mysql+mysqlconnector://"root"+@localhost:3306/Project"
 )
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
